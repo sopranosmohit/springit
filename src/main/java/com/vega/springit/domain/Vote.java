71de8b916236056d566ed7a999504bffb38c4b4c
Mohit Soni <sopranosmohit@gmail.com>
2019-07-14 22:35:05 +0530

    Commit and Refactored Using Lombok

---
+++ src/main/java/com/vega/springit/domain/Vote.java
@@ -0,0 +1,19 @@
+package com.vega.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import java.util.Objects;
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int vote;
+}
